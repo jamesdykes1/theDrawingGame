2b928f1cf83571798f14b57df97edefb203fbeb7
jamesdykes1 <up784157@myport.ac.uk>
2018-01-24 12:07:26 +0000

    Added check function for words guessed and created

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var app = express();
 var server = app.listen(3000);
 
+var drawingName = '';
+var allGuesses = [];
 
 app.set('view engine', 'ejs');
 //app.use(express.static('public'));
@@ -20,13 +22,13 @@ var io = socket(server);
 io.sockets.on('connection', newConnection);
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.post('/player', urlencodedParser, function (req, res) {
-	
+
   if (!req.body) return res.sendStatus(400)
   console.log(req.body);
 	res.render('game', {data: req.body});
@@ -37,9 +39,8 @@ app.post('/player', urlencodedParser, function (req, res) {
 
 //app.get('/Player', function(req, res){
 //res.sendFile(__dirname + '/public/game.html', + req.body.userid);
-	
-//});
 
+//});
 
 
 
@@ -48,6 +49,8 @@ function newConnection(socket){
   console.log('new connection: ' + socket.id);
 
   socket.on('mouse', mouseMsg);
+	socket.on('word', nameOfDrawing);
+	socket.on('guess', listOfGuesses);
 
   /*
       Send X and Y of mouse to other clients
@@ -57,5 +60,24 @@ function newConnection(socket){
     socket.broadcast.emit('mouse', data);
     console.log(data);
   }
+
+	function nameOfDrawing(data){
+		drawingName = data;
+		console.log(drawingName);
+	}
+
+	function listOfGuesses(data){
+		allGuesses.push(data);
+		console.log(allGuesses);
+		for (var i = 0; i < allGuesses.length; i++) {
+			if (allGuesses[i] == drawingName) {
+				console.log('Correct!');
+			}
+			else {
+				console.log('Incorrect.');
+				allGuesses.splice(i, 1);
+			}
+		}
+	}
 
 }
