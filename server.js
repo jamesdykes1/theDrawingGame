c93196ce46c66d452e3d46b3046fe24afa903292
jamesdykes1 <up784157@myport.ac.uk>
2018-02-05 20:08:05 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(8080);
+var addRequestId = require('express-request-id')();
+var session = require('express-session');
+var parseurl = require('parseurl');
 
-var drawingName = '';
-var allGuesses = [];
+var socket = require('socket.io');
+var io = socket(server);
+var cred = require('./cred.js');
+
 
 app.set('view engine', 'ejs');
-//app.use(express.static('public'));
+
 
 console.log("Server Running");
 
@@ -16,14 +22,25 @@ console.log("Server Running");
     Creating the connection to the WebSocket
  */
 
-var socket = require('socket.io');
-var io = socket(server);
+var serverReq = 0;
+var session;
+var clue= [];
+
+http.createServer(function(req, res){
+
+	serverReq++;
+
+	console.log(vis);
+});
+
 
 io.sockets.on('connection', newConnection);
-
+app.use(addRequestId);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('cookie-parser') (cred.cookieSecret));
+app.use(session({secret: 'drawinggame',saveUninitialized: true,resave: false}));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -31,17 +48,60 @@ app.post('/player', urlencodedParser, function (req, res) {
 
   if (!req.body) return res.sendStatus(400)
   console.log(req.body);
-	res.render('game', {data: req.body});
+  console.log(req.cookies);
+  console.log(req.session);
+  console.log(req.sessionID);
+  console.log('request '+serverReq);
 
-	//res.sendFile(__dirname + '/public/game.html', + req.body.userid);
-	//res.json(req.body.userid);
-})
+	clue += req.body.clueword;
+	session = req.session;
+	session.username = req.body.username;
+	//session.clueword = req.body.clueword;
 
-//app.get('/Player', function(req, res){
-//res.sendFile(__dirname + '/public/game.html', + req.body.userid);
+		if (serverReq == 0) {
+			res.render('game', {data: req.body});
+			serverReq++;
+			console.log(serverReq);
+			}
+			else {
+				res.render('incorrectguess', {data: req.body});
+				serverReq++;
+				console.log(serverReq);
+			}
 
-//});
+	});
+var message = '';
 
+app.post('/guesser', urlencodedParser, function (req, res) {
+	  if (!req.body) return res.sendStatus(400)
+
+	  guessedWord = req.body.guessedWord;
+		console.log (guessedWord);
+		console.log (clue);
+	  if (clue == guessedWord) {
+		  //console.log ('Correct');
+		  message = 'correct';
+		  console.log (message);
+		  res.render('correctguess', {data: req.body});
+	  } else {
+			message = 'Incorrect';
+		 	 // console.log ('Incorrect');
+			  console.log (message);
+			  res.render('incorrectguess', {data: req.body});
+	  }
+
+});
+
+/*
+app.get('/guesser', function(req,res){
+
+	session = req.session;
+
+});
+
+
+
+*/
 
 
 
@@ -49,8 +109,6 @@ function newConnection(socket){
   console.log('new connection: ' + socket.id);
 
   socket.on('mouse', mouseMsg);
-	socket.on('word', nameOfDrawing);
-	socket.on('guess', listOfGuesses);
 
   /*
       Send X and Y of mouse to other clients
@@ -61,7 +119,8 @@ function newConnection(socket){
     console.log(data);
   }
 
-	function nameOfDrawing(data){
+  /*
+  	function nameOfDrawing(data){
 		drawingName = data;
 		console.log(drawingName);
 	}
@@ -70,6 +129,10 @@ function newConnection(socket){
 		allGuesses.push(data);
 		console.log(allGuesses);
 		for (var i = 0; i < allGuesses.length; i++) {
+
+
+
+
 			if (allGuesses[i] == drawingName) {
 				console.log('Correct!');
 			}
@@ -79,5 +142,10 @@ function newConnection(socket){
 			}
 		}
 	}
+
+  */
+
+
+
 
 }
