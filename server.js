ce12ca5c3adf435857022240d064072b27b67dce
jamesdykes1 <up784157@myport.ac.uk>
2018-02-11 15:45:43 +0000

    added code to allow css file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var addRequestId = require('express-request-id')();
 var session = require('express-session');
 var parseurl = require('parseurl');
 var stringSimilarity = require('string-similarity');
- 
+
 var socket = require('socket.io');
 var io = socket(server);
 var cred = require('./cred.js');
@@ -27,22 +27,23 @@ var clue= [];
 var player = new Array;
 var playerscore = new Array;
 http.createServer(function(req, res){
-	
+
 	serverReq++;
-	
+
 	console.log(vis);
 });
- 
+
 
 io.sockets.on('connection', newConnection);
 app.use(addRequestId);
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/views'));
 
 
 //app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require('cookie-parser') (cred.cookieSecret));
 app.use(session({secret: 'drawinggame',saveUninitialized: true,resave: false}));
@@ -50,42 +51,42 @@ app.use(session({secret: 'drawinggame',saveUninitialized: true,resave: false}));
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', function(req,res){
-	
+
 	res.render('index', {data: {serverRequest: serverReq}});
 
 });
 
 
 app.post('/player', urlencodedParser, function (req, res) {
-	
+
   if (!req.body) return res.sendStatus(400)
   console.log(req.body);
   console.log(req.cookies);
   console.log(req.session);
-  console.log(req.sessionID);  
+  console.log(req.sessionID);
   console.log('request '+serverReq);
 
-	clue += req.body.clueword; 
-	
+	clue += req.body.clueword;
+
 	session = req.session;
 	session.username = req.body.username;
 	session.clueword = req.body.clueword;
 	console.log(req.session);
-					
+
 		if (serverReq == 0) {
-			res.render('game', 
+			res.render('game',
 			{
 					data: {
 						username: req.session.username
 						//score: scores
 					}
 				}
-			);	
+			);
 			serverReq++;
 			console.log(serverReq);
 			}
 			else {
-				res.render('correctguess', 
+				res.render('correctguess',
 				{
 					data: {
 						username: req.session.username
@@ -99,18 +100,18 @@ app.post('/player', urlencodedParser, function (req, res) {
 
 
 	});
-	
+
 app.post('/guesser', urlencodedParser, function (req, res) {
 	console.log(req.session);
 	  if (!req.body) return res.sendStatus(400)
 		if  (req.sessionID){
 		  guessedWord = req.body.guessedWord;
-		  
+
 			console.log (guessedWord);
 			console.log (clue);
-			
+
 		scores = stringSimilarity.compareTwoStrings(clue,guessedWord);
-				for (var i = 0; i < 1; i++){  
+				for (var i = 0; i < 1; i++){
 
 					player.push(req.session.username);
 					playerscore.push(scores);
@@ -118,7 +119,7 @@ app.post('/guesser', urlencodedParser, function (req, res) {
 			console.log(player);
 			console.log(playerscore);
 				console.log(scores)
-			  res.render('correctguess', 
+			  res.render('correctguess',
 			  {data: {
 						username: req.session.username,
 						score: scores,
@@ -129,7 +130,7 @@ app.post('/guesser', urlencodedParser, function (req, res) {
 			   guessedWord = [];
 
 		}
-});	
+});
 
 
 
